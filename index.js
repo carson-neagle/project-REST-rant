4c1e1fbe0b489eadd99813770f6fbc99b6619f5f
Carson Neagle <carson@Carsons-MacBook-Pro.local>
2022-07-05 19:01:26 -0400

    Adding styles to rest-rant

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
-app.use('/places', require('./controllers/places'))
+//Controllers & Routes
+app.use('/places', require('./controllers/places.js'))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -15,4 +19,5 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
+//Listen for Connections
 app.listen(process.env.PORT)
