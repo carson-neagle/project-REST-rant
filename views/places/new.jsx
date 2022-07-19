6784c207dbec94c5d49360aa2055a7ba866b9872
Carson Neagle <carson@Carsons-MacBook-Pro.local>
2022-07-18 20:43:59 -0400

    part 8 changes

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -30,6 +30,10 @@ function new_form () {
                     <div>
                         <input className="btn btn-primary" type="submit" value="Add Place"/>
                     </div>
+                    <div className="form-group">
+                        <label for="founded">Founded Year</label>
+                        <input className="form-control" id="founded" name="founded" />
+                    </div>
                 </form>
             </main>
         </Def>
