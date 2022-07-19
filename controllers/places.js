cde3194b4424f44b9d1a37f14931d5fbf3ccdad8
Carson Neagle <carson@Carsons-MacBook-Pro.local>
2022-07-19 19:16:56 -0400

    part 9 changes

--- controllers/places.js
+++ controllers/places.js
@@ -19,13 +19,23 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
   db.Place.create(req.body)
   .then(() => {
-    res.redirect('/places')
+      res.redirect('/places')
   })
-  .catch( err => {
-    console.log('err', err)
-    res.render('error404')
+  .catch(err => {
+      if (err && err.name == 'ValidationError') {
+          let message = 'Validation Error:'
+          for (var field in err.errors) {
+            message += `${field} was ${err.errors[field].value}`
+            message += `${err.errors[field].message}`
+          }
+          res.render('/places/new', { message })
+      }
+      else {
+          res.render('error404')
+      }
   })
 })
+
 
 router.get('/new', (req, res) => {
   res.render('places/new')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,9 +2,38 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let message = ''
+    if (data.message) {
+        message = (
+            <h4 className="alert-danger">
+                {data.message}
+            </h4>
+        )
+    }
     return (
         <Def>
             <main>
+                <h1>Add a New Place</h1>
+                {message}
+                <div className="row">
+                    <div className="col-sm-6">
+                        <img src={data.place.pic} alt={data.place.name}/>
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <h2>
+                        Description
+                    </h2>
+                    <h3>
+                        {data.place.showEstablished()}
+                    </h3>
+                    <h4>
+                        Serving {data.place.cuisines}
+                    </h4>
+                </div>
                 <h1>{ data.place.name }</h1>
                 <a href={`/places/${data.id}/edit`} className="btn-warning">
                     Edit
