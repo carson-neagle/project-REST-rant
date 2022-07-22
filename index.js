cdfafc208e11482b1ca1b72f17ed1afc480b7247
Carson Neagle <carson@Carsons-MacBook-Pro.local>
2022-07-21 20:46:37 -0400

    organization and bug fixes

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-require('dotenv').config()
-const express = require('express')
-const methodOverride = require('method-override')
-const app = express()
+// Modules and Globals
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const methodOverride = require("method-override");
 
 // Express Settings
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+app.set("views", __dirname + "/views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
-//Controllers & Routes
-app.use('/places', require('./controllers/places.js'), express.urlencoded({extended: true}))
+// Controllers & Routes
+app.use("/places", require("./controllers/places"));
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get('*', (req, res) => {
-    res.render('error404')
-})
+app.get("*", (req, res) => {
+  res.render("error404");
+});
 
-//Listen for Connections
-app.listen(process.env.PORT)
+// Listen for Connections
+app.listen(process.env.PORT);
